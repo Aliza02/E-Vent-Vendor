ddcc844ecf03015198e23e800be24bf01dd33e40
Aliza02 <alizaanxari08@gmail.com>
2023-05-27 21:20:52 +0500

    getx variable

---
+++ lib/controller/pagecontroller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class testController extends GetxController {
+  var currentindex = 0.obs;
+
+  void changeIndex(int index) {
+    currentindex.value = index;
+    print(currentindex.value);
+  }
+}
--- lib/screens/drawer/mainScreen.dart
+++ lib/screens/drawer/mainScreen.dart
@@ -3,134 +3,32 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
 
 import '../../constants/colors.dart';
 import '../../constants/icons.dart';
+import '../../controller/pagecontroller.dart';
 
-class MainScreen extends StatefulWidget {
-  final drawerController;
-  MainScreen({super.key, this.drawerController});
+class MainScreen extends GetView<testController> {
+  MainScreen({
+    super.key,
+  });
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  List<String> navBarIcon = [
-    AppIcons.home,
-    AppIcons.message,
-    AppIcons.addButton,
-    AppIcons.calender,
-    AppIcons.profile
-  ];
-  bool isOpen = false;
-  int currentindex = 0;
-  bool selected = false;
-  void initstate() {
-    super.initState();
-  }
-
-  void dispose() {
-    super.dispose();
-    currentindex = 0;
-  }
+  final pagecontroller = Get.put(testController());
 
   final List<Widget> _pages = [
     pageOne(),
     pageTwo(),
     pageThree(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        bottomNavigationBar: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.08,
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: AppColors.pink,
-                    width: 2.0,
-                  ),
-                ),
-                gradient: LinearGradient(
-                    transform: GradientRotation(0.0),
-                    colors: [
-                      AppColors.screenBackground.withOpacity(0.1),
-                      AppColors.pink.withOpacity(0.4),
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter),
-              ),
-            ),
-            BottomAppBar(
-              color: Colors.transparent,
-              elevation: 0.0,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height * 0.01),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          currentindex = 0;
-                        });
-                      },
-                      icon: currentindex == 0
-                          ? SvgPicture.asset(AppIcons.homeFill)
-                          : SvgPicture.asset(AppIcons.home),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          currentindex = 1;
-                        });
-                      },
-                      icon: currentindex == 1
-                          ? SvgPicture.asset(AppIcons.messageFill)
-                          : SvgPicture.asset(AppIcons.message),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          currentindex = 2;
-                        });
-                      },
-                      icon: currentindex == 2
-                          ? SvgPicture.asset(AppIcons.addButtonFill)
-                          : SvgPicture.asset(AppIcons.addButton),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          currentindex = 3;
-                        });
-                      },
-                      icon: currentindex == 3
-                          ? SvgPicture.asset(AppIcons.calendarFill)
-                          : SvgPicture.asset(AppIcons.calender),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        setState(() {
-                          currentindex = 4;
-                        });
-                      },
-                      icon: currentindex == 4
-                          ? SvgPicture.asset(AppIcons.profileFill)
-                          : SvgPicture.asset(AppIcons.profile),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+        bottomNavigationBar: bottomNavBar(),
         appBar: AppBar(
           backgroundColor: AppColors.appBar.withOpacity(0.2),
           shadowColor: Colors.transparent,
@@ -146,7 +44,9 @@ class _MainScreenState extends State<MainScreen> {
             ),
           ),
         ),
-        body: _pages[currentindex],
+        body: Obx(
+          () => _pages[pagecontroller.currentindex.value],
+        ),
       ),
     );
   }
@@ -158,7 +58,7 @@ class pageOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: const Text('Page One'),
+      child: Text('page one'),
     );
   }
 }
@@ -181,6 +81,98 @@ class pageThree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: const Text('Page three'),
+    );
+  }
+}
+
+class bottomNavBar extends GetView<testController> {
+  bottomNavBar({super.key});
+  final pagecontroller = Get.put(testController());
+  int currentindex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.08,
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                color: AppColors.pink,
+                width: 2.0,
+              ),
+            ),
+            gradient: LinearGradient(
+                transform: GradientRotation(0.0),
+                colors: [
+                  AppColors.screenBackground.withOpacity(0.1),
+                  AppColors.pink.withOpacity(0.2),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter),
+          ),
+        ),
+        BottomAppBar(
+          color: Colors.transparent,
+          elevation: 0.0,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.01),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      pagecontroller.changeIndex(0);
+                      print(pagecontroller.currentindex.value);
+                    },
+                    icon: Obx(
+                      () => pagecontroller.currentindex == 0
+                          ? SvgPicture.asset(AppIcons.homeFill)
+                          : SvgPicture.asset(AppIcons.home),
+                    )),
+                IconButton(
+                    onPressed: () {
+                      pagecontroller.changeIndex(1);
+                    },
+                    icon: Obx(
+                      () => pagecontroller.currentindex == 1
+                          ? SvgPicture.asset(AppIcons.messageFill)
+                          : SvgPicture.asset(AppIcons.message),
+                    )),
+                IconButton(
+                    onPressed: () {
+                      pagecontroller.changeIndex(2);
+                    },
+                    icon: Obx(
+                      () => pagecontroller.currentindex == 2
+                          ? SvgPicture.asset(AppIcons.addButtonFill)
+                          : SvgPicture.asset(AppIcons.addButton),
+                    )),
+                IconButton(
+                    onPressed: () {
+                      pagecontroller.changeIndex(3);
+                    },
+                    icon: Obx(
+                      () => pagecontroller.currentindex == 3
+                          ? SvgPicture.asset(AppIcons.calendarFill)
+                          : SvgPicture.asset(AppIcons.calender),
+                    )),
+                IconButton(
+                    onPressed: () {
+                      pagecontroller.changeIndex(4);
+                    },
+                    icon: Obx(
+                      () => pagecontroller.currentindex == 4
+                          ? SvgPicture.asset(AppIcons.profileFill)
+                          : SvgPicture.asset(AppIcons.profile),
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
